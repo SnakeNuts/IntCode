8e3d47f4c913b76c72afbba71c15259572f1f147
SnakeNuts <alexander@crouzen.com>
2023-08-13 21:52:48 +0000

    Changes via replit

--- intcode.py
+++ intcode.py
@@ -1,178 +1,181 @@
 import queue
+import logging
 from collections import defaultdict
+
 
 class IntCodeCPU:
 
-    # setup section
+  # setup section
 
-    def __init__(self, id:int) -> None:
-        self.__pc = 0
-        self.relative_base = 0
-        self.id = id
-        self.memory = defaultdict(int)
+  def __init__(self, id: int) -> None:
+    self.__pc = 0
+    self.relative_base = 0
+    self.id = id
+    self.memory = defaultdict(int)
 
-        self.input_queue = queue.SimpleQueue()
+    self.input_queue = queue.SimpleQueue()
 
-        self.output_value = 0
-        self.output_set = False
-        self.output_stop_mode = False
-    
-    def __str__(self) -> str:
-        return f"CPU [{self.id}] - pc:{self.pc_peek()} - memsize:{len(self.memory)}"
+    self.output_value = 0
+    self.output_set = False
+    self.output_stop_mode = False
 
-    def load_program(self, program_string: str) -> None:
-        load_counter = 0
-        for intvalue in program_string.split(","):
-            self.memory[load_counter] = int(intvalue)
-            load_counter += 1
+  def __str__(self) -> str:
+    return f"CPU [{self.id}] - pc:{self.pc_peek()} - memsize:{len(self.memory)}"
 
-    # registers section
+  def load_program(self, program_string: str) -> None:
+    load_counter = 0
+    for intvalue in program_string.split(","):
+      self.memory[load_counter] = int(intvalue)
+      load_counter += 1
 
-    def pc_peek(self) -> int:
-        return self.__pc
-    
-    def set_pc(self, new_pc_value) -> None:
-        self.__pc = new_pc_value
+  # registers section
 
-    # memory section
+  def pc_peek(self) -> int:
+    return self.__pc
 
-    def next_value(self) -> int:
-        value = self.memory[self.__pc]
-        self.__pc += 1 
-        return value
+  def set_pc(self, new_pc_value) -> None:
+    self.__pc = new_pc_value
 
-    def get_mem(self, address:int) -> int:
-        return self.memory[address]
+  # memory section
 
-    def set_mem(self, address:int, value:int) -> None:
-        self.memory[address] = value
+  def next_value(self) -> int:
+    value = self.memory[self.__pc]
+    self.__pc += 1
+    return value
 
-    def read(self, parameter:int, mode:int) -> int:
-        match mode:
-            case 0:
-                return self.get_mem(parameter)
-            case 1:
-                return parameter
-            case 2:
-                return self.get_mem(self.relative_base + parameter)
-            case _:
-                raise Exception(f"[{self.id}] - Invalid addressing mode '{mode}'")
+  def get_mem(self, address: int) -> int:
+    return self.memory[address]
 
-    def write(self, target, value, mode):
-        match mode:
-            case 0:
-                self.set_mem(target, value)
-            case 1:
-                raise Exception("[{self.id}] - Direct mode for writing not allowed!")
-            case 2:
-                self.set_mem(self.relative_base + target, value)
-            case _:
-                raise Exception(f"[{self.id}] - Invalid addressing mode '{mode}'")
+  def set_mem(self, address: int, value: int) -> None:
+    self.memory[address] = value
 
+  def read(self, parameter: int, mode: int) -> int:
+    match mode:
+      case 0:
+        return self.get_mem(parameter)
+      case 1:
+        return parameter
+      case 2:
+        return self.get_mem(self.relative_base + parameter)
+      case _:
+        raise Exception(f"[{self.id}] - Invalid addressing mode '{mode}'")
 
-    # runtime section
+  def write(self, target, value, mode):
+    match mode:
+      case 0:
+        self.set_mem(target, value)
+      case 1:
+        raise Exception(f"[{self.id}] - Direct mode for writing not allowed!")
+      case 2:
+        self.set_mem(self.relative_base + target, value)
+      case _:
+        raise Exception(f"[{self.id}] - Invalid addressing mode '{mode}'")
 
-    def run(self):
-        stopped = False
-        while not stopped:
-            stopped = self.step()
+  # runtime section
 
-    def step(self) -> bool:
-        opcode_struct = self.next_value()
+  def run(self):
+    stopped = False
+    while not stopped:
+      stopped = self.step()
 
-        # parse parameter modes
-        opcode = (opcode_struct % 10) + ((opcode_struct % 100) // 10) * 10
-        param1_mode = (opcode_struct % 1000) // 100
-        param2_mode = (opcode_struct % 10000) // 1000
-        param3_mode = (opcode_struct % 100000) // 10000 
+  def step(self) -> bool:
+    opcode_struct = self.next_value()
 
+    # parse parameter modes
+    opcode = (opcode_struct % 10) + ((opcode_struct % 100) // 10) * 10
+    param1_mode = (opcode_struct % 1000) // 100
+    param2_mode = (opcode_struct % 10000) // 1000
+    param3_mode = (opcode_struct % 100000) // 10000
 
-        match opcode:
-            case 1:
-                self.opcode_add(param1_mode, param2_mode, param3_mode)
-            case 2:
-                self.opcode_multiply(param1_mode, param2_mode, param3_mode)
-            case 3:
-                self.opcode_input(param1_mode)
-            case 4:
-                self.opcode_output(param1_mode)
-                if self.output_stop_mode:
-                    print(f"[{self.id}] - stopping after output")
-                    return True
-            case 5:
-                self.opcode_jump_if_true(param1_mode, param2_mode)
-            case 6:
-                self.opcode_jump_if_false(param1_mode, param2_mode)
-            case 7:
-                self.opcode_less_than(param1_mode, param2_mode, param3_mode)
-            case 8:
-                self.opcode_equals(param1_mode, param2_mode, param3_mode)
-            case 9:
-                self.opcode_set_relative_base(param1_mode)
-            case 99:
-                print(f"[{self.id}] - program done")
-                return True
-            case _:
-                raise Exception(f"[{self.id}] - Invalid opcode '{opcode}'")
-        return False
-    
-    def opcode_add(self, param1_mode, param2_mode, param3_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        target = self.next_value()
-        self.write(target, source_1 + source_2, param3_mode)
+    match opcode:
+      case 1:
+        self.opcode_add(param1_mode, param2_mode, param3_mode)
+      case 2:
+        self.opcode_multiply(param1_mode, param2_mode, param3_mode)
+      case 3:
+        self.opcode_input(param1_mode)
+      case 4:
+        self.opcode_output(param1_mode)
+        if self.output_stop_mode:
+          logging.info(f"[{self.id}] - stopping after output")
+          return True
+      case 5:
+        self.opcode_jump_if_true(param1_mode, param2_mode)
+      case 6:
+        self.opcode_jump_if_false(param1_mode, param2_mode)
+      case 7:
+        self.opcode_less_than(param1_mode, param2_mode, param3_mode)
+      case 8:
+        self.opcode_equals(param1_mode, param2_mode, param3_mode)
+      case 9:
+        self.opcode_set_relative_base(param1_mode)
+      case 99:
+        logging.info(f"[{self.id}] - program done")
+        return True
+      case _:
+        raise Exception(f"[{self.id}] - Invalid opcode '{opcode}'")
+    return False
 
-    def opcode_multiply(self, param1_mode, param2_mode, param3_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        target = self.next_value()
-        self.write(target, source_1 * source_2, param3_mode)
+  def opcode_add(self, param1_mode, param2_mode, param3_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    target = self.next_value()
+    self.write(target, source_1 + source_2, param3_mode)
 
-    def opcode_input(self, param1_mode) -> None:
-        target = self.next_value()
-        if self.input_queue.empty():
-            value = int(input(f"[{self.id}] - Please enter an integer to be stored at {target}:"))
-        else:
-            value = self.input_queue.get()
-            print(f"[{self.id}] - Read {value} from input queue into {target}")
-        self.write(target, value, param1_mode)
+  def opcode_multiply(self, param1_mode, param2_mode, param3_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    target = self.next_value()
+    self.write(target, source_1 * source_2, param3_mode)
 
-    def opcode_output(self, param1_mode) -> None:
-        value = self.read(self.next_value(), param1_mode)
-        print(f"[{self.id}] - output: {value}")
-        self.output_value = value
-        self.output_set = True
+  def opcode_input(self, param1_mode) -> None:
+    target = self.next_value()
+    if self.input_queue.empty():
+      value = int(
+        input(
+          f"[{self.id}] - Please enter an integer to be stored at {target}:"))
+    else:
+      value = self.input_queue.get()
+      logging.info(
+        f"[{self.id}] - Read {value} from input queue into {target}")
+    self.write(target, value, param1_mode)
 
-    def opcode_jump_if_true(self, param1_mode, param2_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        if source_1 != 0:
-            self.set_pc(source_2)
-        
-    def opcode_jump_if_false(self, param1_mode, param2_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        if source_1 == 0:
-            self.set_pc(source_2)
+  def opcode_output(self, param1_mode) -> None:
+    value = self.read(self.next_value(), param1_mode)
+    logging.info(f"[{self.id}] - output: {value}")
+    self.output_value = value
+    self.output_set = True
 
-    def opcode_less_than(self, param1_mode, param2_mode, param3_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        target = self.next_value()
-        if source_1 < source_2:
-            self.write(target, 1, param3_mode)
-        else:
-            self.write(target, 0, param3_mode)
+  def opcode_jump_if_true(self, param1_mode, param2_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    if source_1 != 0:
+      self.set_pc(source_2)
 
-    def opcode_equals(self, param1_mode, param2_mode, param3_mode) -> None:
-        source_1 = self.read(self.next_value(), param1_mode)
-        source_2 = self.read(self.next_value(), param2_mode)
-        target = self.next_value()
-        if source_1 == source_2:
-            self.write(target, 1, param3_mode)
-        else:
-            self.write(target, 0, param3_mode)
+  def opcode_jump_if_false(self, param1_mode, param2_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    if source_1 == 0:
+      self.set_pc(source_2)
 
-    def opcode_set_relative_base(self, param1_mode) -> None:
-        adjustment = self.read(self.next_value(), param1_mode)
-        self.relative_base += adjustment
+  def opcode_less_than(self, param1_mode, param2_mode, param3_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    target = self.next_value()
+    if source_1 < source_2:
+      self.write(target, 1, param3_mode)
+    else:
+      self.write(target, 0, param3_mode)
+
+  def opcode_equals(self, param1_mode, param2_mode, param3_mode) -> None:
+    source_1 = self.read(self.next_value(), param1_mode)
+    source_2 = self.read(self.next_value(), param2_mode)
+    target = self.next_value()
+    if source_1 == source_2:
+      self.write(target, 1, param3_mode)
+    else:
+      self.write(target, 0, param3_mode)
+
+  def opcode_set_relative_base(self, param1_mode) -> None:
+    adjustment = self.read(self.next_value(), param1_mode)
+    self.relative_base += adjustment
--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 from intcode import IntCodeCPU
+import logging
+import itertools
 
 with open("input_7.txt") as input:
-    code = input.readline()
+  code = input.readline()
 
-    amp_A = IntCodeCPU(0)
-    amp_A.load_program(code)
-    amp_B = IntCodeCPU(1)
-    amp_B.load_program(code)
-    amp_C = IntCodeCPU(2)
-    amp_C.load_program(code)
-    amp_D = IntCodeCPU(3)
-    amp_D.load_program(code)
-    amp_E = IntCodeCPU(4)
-    amp_E.load_program(code)
+  amp_A = IntCodeCPU(0)
+  amp_A.load_program(code)
+  amp_B = IntCodeCPU(1)
+  amp_B.load_program(code)
+  amp_C = IntCodeCPU(2)
+  amp_C.load_program(code)
+  amp_D = IntCodeCPU(3)
+  amp_D.load_program(code)
+  amp_E = IntCodeCPU(4)
+  amp_E.load_program(code)
 
 
-def run_all_amplifiers(initial_signal, phase_A, phase_B, phase_C, phase_D, phase_E):
-    amp_A.input_queue.put(phase_A)
-    amp_A.input_queue.put(initial_signal)
-    amp_A.run()
+def run_all_amplifiers(initial_signal, phase_A, phase_B, phase_C, phase_D,
+                       phase_E):
+  amp_A.input_queue.put(phase_A)
+  amp_A.input_queue.put(initial_signal)
+  amp_A.run()
 
-    amp_B.input_queue.put(phase_B)
-    amp_B.input_queue.put(amp_A.output_value)
-    amp_B.run()
-        
-    amp_C.input_queue.put(phase_C)
-    amp_C.input_queue.put(amp_B.output_value)
-    amp_C.run()
+  amp_B.input_queue.put(phase_B)
+  amp_B.input_queue.put(amp_A.output_value)
+  amp_B.run()
 
-    amp_D.input_queue.put(phase_D)
-    amp_D.input_queue.put(amp_C.output_value)
-    amp_D.run()
+  amp_C.input_queue.put(phase_C)
+  amp_C.input_queue.put(amp_B.output_value)
+  amp_C.run()
 
-    amp_E.input_queue.put(phase_E)
-    amp_E.input_queue.put(amp_D.output_value)
-    amp_E.run()  
+  amp_D.input_queue.put(phase_D)
+  amp_D.input_queue.put(amp_C.output_value)
+  amp_D.run()
 
-    return amp_E.output_value   
+  amp_E.input_queue.put(phase_E)
+  amp_E.input_queue.put(amp_D.output_value)
+  amp_E.run()
+
+  return amp_E.output_value
 
 
 def main():
-    print(run_all_amplifiers(0, 0, 1, 2, 3, 4))
- 
-        
-
+  phases = [0, 1, 2, 3, 4]
+  phases_permitations = list(itertools.permutations(phases, 5))
+  for permutation in phases_permitations:
+    print(run_all_amplifiers(0, *permutation))
+    # cpus need resetting in this phase of the problem
 
 
 if __name__ == "__main__":
-    main()
+  logging.basicConfig(level=logging.INFO)
+  #numberslist = [0,1,2,3,4]
+  #print(list(itertools.permutations(numberslist,5)))
+  main()
